c3666c2458c56958a47c4b44db2fad1ba241ba01
Khkimov <khakim198765@gmail.com>
2023-03-14 22:50:14 +0500

    created components

---
+++ src/components/CategoriesItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <a href="#" :class="classes">
+    {{ props.category }}
+  </a>
+</template>
+
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  category: {
+    type: Array
+  },
+  isActive: {
+    type: Boolean
+  }
+})
+
+const classes = computed(() => {
+  const classes = ['px-3', 'py-1', 'transition', 'rounded-full', 'border']
+  return props.isActive
+      ? [
+        ...classes,
+        'border-gray-700',
+        'bg-gray-600',
+        'hover:bg-gray-500',
+        'text-white'
+      ]
+      : [
+        ...classes,
+        'border-gray-300',
+        'bg-gray-100',
+        'hover:bg-gray-200'
+      ]
+})
+
+</script>
---
+++ src/components/TheCategories.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <section class="pt-14 md:pl-24 xl:pl-64 w-full fixed bg-white bg-opacity-95 z-10">
+      <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
+        <div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap">
+          <CategoriesItem
+              v-for="category in categories"
+              :key="category"
+              :category="category"
+              :is-active="category === 'All'"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import CategoriesItem from "./CategoriesItem.vue";
+
+const categories = ref([
+  'All',
+  'Trucks',
+  'Tools',
+  'Machines',
+  'Engines',
+  'Snow',
+  'Ships',
+  'Roads',
+  'Tanks',
+  'Building',
+  'Mars',
+  'Woodworking',
+  'Tractors',
+  'Songs',
+  'TV Shows',
+  'Football',
+  'Planes',
+  'Live',
+  'Streets',
+  'Math',
+  'Programming'
+])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
