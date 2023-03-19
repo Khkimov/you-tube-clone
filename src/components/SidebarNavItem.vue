5c39030f129c836dec2ffa51b5a8fa2b11bdd334
Khkimov <khakim198765@gmail.com>
2023-03-19 15:28:22 +0500

    created components for the sidebar

---
+++ src/components/SidebarNavItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <a href="#" :class="classes">
+      <BaseIcon :name="props.icon" :class="iconClasses"/>
+      <span>{{ props.label }}</span>
+    </a>
+  </li>
+</template>
+
+<script setup>
+import BaseIcon from "./BaseIcon.vue";
+import {computed} from "vue";
+
+const props = defineProps({
+  label: {
+    type: String
+  },
+  icon: {
+    type: String
+  },
+  isActive: {
+    type: Boolean
+  }
+})
+
+const classes = computed(() => {
+  const classes = ['flex', 'items-center', 'px-6', 'py-2', 'text-sm']
+  return props.isActive
+  ? [
+        ...classes,
+        'font-medium',
+        'text-gray-800',
+        'bg-gray-200',
+        'hover:bg-gray-300'
+      ]
+      : [...classes, 'hover:bg-gray-100']
+})
+
+const iconClasses = computed(() => {
+  return props.isActive ? 'w-6 h-6 mr-6 text-red-500' : 'w-6 h-6 mr-6'
+})
+</script>
---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex items-start mt-3">
+    <img :src="`https://picsum.photos/seed/avatar${props.index}/68`" class="mr-3 rounded-full w-9 h-9" alt="">
+    <div class="text-sm">
+      <span class="font-semibold text-gray-800">Video title</span>
+      <div class="mt-1 flex items-center">
+        <span>Channel name {{ props.index }}</span>
+        <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
+      </div>
+      <div v-html="summary"></div>
+    </div>
+    <button class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none">
+      <BaseIcon name="dotsVertical" />
+    </button>
+  </div>
+</template>
+
+<script setup>
+import BaseIcon from "./BaseIcon.vue";
+import {computed} from "vue";
+
+const props = defineProps({
+  index: {
+    type: Number
+  }
+})
+
+const summary = computed(() => {
+  const days = props.index === 1 ? 'day' : 'days'
+  return `${props.index}K views &middot; ${ props.index } ${days} ago`
+})
+</script>
