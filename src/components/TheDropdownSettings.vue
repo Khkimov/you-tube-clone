497e73315af7e22c8fb71b6fb3f83b66b926ae94
Khkimov <khakim198765@gmail.com>
2023-04-06 03:41:33 +0500

    Dropdown menu custom

--- src/components/TheDropdownSettings.vue
+++ src/components/TheDropdownSettings.vue
@@ -1,26 +1,45 @@
 <template>
-  <div class="opacity-0 group-hover:opacity-100 absolute top-9 -right-full sm:right-0 bg-white w-72 border border-t-0">
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownSettingsListItem
-            v-for="listItem in listItems.slice(0, 8)"
-            :key="listItem.label"
-            :label="listItem.label"
-            :icon="listItem.icon"
-            :with-sub-menu="listItem.withSubMenu" />
-      </ul>
-    </section>
-    <section class="py-2">
-      <ul>
-        <DropdownSettingsListItem :label="listItems[8].label" :with-sub-menu="listItems[8].withSubMenu"  />
-      </ul>
-    </section>
+  <div class="relative" ref="root">
+    <button
+        @click="isOpen = !isOpen"
+        class="relative p-2 focus:outline-none">
+      <BaseIcon name="dotsVertical" class="w-5 h-5"/>
+    </button>
+    <transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transition opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95"
+    >
+      <div
+          v-show="isOpen"
+          class="absolute top-9 -right-full sm:right-0 bg-white w-72 border border-t-0">
+        <section class="py-2 border-b">
+          <ul>
+            <DropdownSettingsListItem
+                v-for="listItem in listItems.slice(0, 8)"
+                :key="listItem.label"
+                :label="listItem.label"
+                :icon="listItem.icon"
+                :with-sub-menu="listItem.withSubMenu"/>
+          </ul>
+        </section>
+        <section class="py-2">
+          <ul>
+            <DropdownSettingsListItem :label="listItems[8].label" :with-sub-menu="listItems[8].withSubMenu"/>
+          </ul>
+        </section>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
 import DropdownSettingsListItem from "./DropdownSettingsListItem.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import BaseIcon from "./BaseIcon.vue";
 
 const listItems = ref([
   {
@@ -69,4 +88,15 @@ const listItems = ref([
     withSubMenu: true
   }
 ])
+
+const isOpen = ref(false)
+const root = ref(null)
+
+onMounted(() => {
+  window.addEventListener('click', event => {
+    if (!root.value.contains(event.target)) {
+      isOpen.value = false
+    }
+  })
+})
 </script>
