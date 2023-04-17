88487320e3d9acfee5278af562587c5513a04d4a
Khkimov <khakim198765@gmail.com>
2023-04-18 03:21:34 +0500

    create base checkbox component

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="checkbox"
+    :id="props.id"
+    v-bind="$attrs"
+    v-model="props.modelValue"
+    class="h-5 w-5 cursor-pointer"
+  />
+  <label v-if="$slots.default" :for="props.id" class="pl-4 cursor-pointer flex-grow">
+    <slot />
+  </label>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: Number,
+  modelValue: Array
+})
+</script>
--- src/components/TheSearchPredictionsList.vue
+++ src/components/TheSearchPredictionsList.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="space-y-3 text-black text-sm mt-5">
     <div v-for="(label, index) in props.searchPredictions" :key="label" class="flex items-center">
-      <input
-        type="checkbox"
-        class="h-5 w-5 cursor-pointer"
-        :id="index"
-        :value="label"
-        v-bind="$attrs"
-        v-model="props.modelValue"
-      />
-      <label :for="index" class="pl-4 cursor-pointer flex-grow">
+      <BaseCheckbox :id="index" :value="label" v-bind="$attrs" v-model="props.modelValue">
         {{ label }}
-      </label>
+      </BaseCheckbox>
     </div>
   </div>
 </template>
 
 <script setup>
+import BaseCheckbox from './BaseCheckbox.vue'
+
 const props = defineProps({
   searchPredictions: Array,
   modelValue: Array
