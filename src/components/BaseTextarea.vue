eca79cb6761226d2fae903dc16b275f191eb4785
Khkimov <khakim198765@gmail.com>
2023-04-19 02:40:38 +0500

    add textarea counter

--- src/components/BaseTextarea.vue
+++ src/components/BaseTextarea.vue
@@ -1,18 +1,42 @@
 <template>
-  <textarea
-    v-bind="$attrs"
-    v-model="modelValue"
-    placeholder="Provide additional details (optional)"
-    class="border rounded-sm pt-2 pb-5 px-2 resize-none w-full focus:outline-none"
-  />
+  <div class="relative">
+    <textarea
+      v-bind="$attrs"
+      v-model="modelValue"
+      @input="adjustHeight"
+      :maxlength="limit"
+      ref="textarea"
+      placeholder="Provide additional details (optional)"
+      class="border rounded-sm pt-2 pb-8 px-2 resize-none w-full focus:outline-none overflow-hidden"
+    />
+    <BaseTextareaCounter>
+      {{ counter }}
+    </BaseTextareaCounter>
+  </div>
 </template>
 
 <script setup>
-import { toRefs } from 'vue'
+import { computed, onMounted, ref, toRefs } from 'vue'
+import BaseTextareaCounter from './BaseTextareaCounter.vue'
 
 const props = defineProps({
-  modelValue: String
+  modelValue: String,
+  limit: Number
 })
 
-const { modelValue } = toRefs(props)
+const { modelValue, limit } = toRefs(props)
+const textarea = ref(null)
+
+const adjustHeight = () => {
+  textarea.value.style.height = 'auto'
+  textarea.value.style.height = `${textarea.value.scrollHeight}px`
+}
+
+onMounted(() => {
+  adjustHeight()
+})
+
+const counter = computed(() => {
+  return `${modelValue.value.length} / ${limit.value}`
+})
 </script>
--- src/components/TheModalSearchPredictions.vue
+++ src/components/TheModalSearchPredictions.vue
@@ -11,7 +11,7 @@
       :categories="searchPredictionCategories"
     />
     <div class="text-black text-sm mt-6">
-      <BaseTextarea v-model="searchPredictionDetails" />
+      <BaseTextarea v-model="searchPredictionDetails" :limit="500" />
     </div>
     <template #footer="{ close }">
       <BaseButton @click="close" class="ml-auto">Cancel</BaseButton>
